2d089c543161799b83ff00e51ec33a1f6ae2570d
Khadija288 <khadijarani289@gmail.com>
2025-08-18 23:05:24 +0500

    edit

--- script.js
+++ script.js
@@ -113,7 +113,7 @@
                         if (filter === 'all' || card.getAttribute('data-category') === filter) {
                             card.style.display = 'block';
                         } else {
-                            card.style.display = 'none'
+                            card.style.display = 'none';
                         }
                     });
                 });
